d5804920d96664cc6239c6c0fbdc5a7402258eb3
Tobias <xyaren@users.noreply.github.com>
2021-06-02 17:57:37 +0200

    use load as default

--- lpassssh/args.py
+++ lpassssh/args.py
@@ -5,9 +5,10 @@ from typing import Tuple
 
 def parse_args() -> Tuple[Namespace, argparse.ArgumentParser]:
     parser: argparse.ArgumentParser = argparse.ArgumentParser(description='lpass-ssh')
-    subparsers = parser.add_subparsers(required=True, dest='subparser_name')
+    subparsers = parser.add_subparsers(required=False, dest='subparser_name')
 
     list_parser = subparsers.add_parser("list", help="List available SSH Secrets from LastPass")
     list_parser.add_argument("--table", help="Display as table", action='store_true')
     subparsers.add_parser("load", help="Load available SSH Secrets into the ssh agent.")
+    parser.set_defaults(subparser_name="load")
     return parser.parse_args(), parser
